59db9e13384f4cdee275e535acd59bc60114da83
viluong-pct <vi.luong@x-factory.jp>
2025-06-16 23:20:29 +0700

    add first test case

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from "@playwright/test";
 
 test("should load homepage", async ({ page }) => {
-  await page.goto("/");
-  await expect(page).toHaveTitle(/Your App/); // sửa cho đúng app của bạn
+  await page.goto("/talent/issue/feature/4/");
+  await expect(page).toHaveTitle(
+    /副業・フリーランス案件特集 | 求む！挑戦者。 スタートアップ案件特集/
+  );
 });
